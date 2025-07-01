855a79669ba32f123b0684a673bd65c40138cec9
Patrik Prinz <patrik.prinz@patryk.cz>
2025-07-01 12:30:48 +0200

    Update: docker and server config
    root:
      - Add docker compose and server package configuration
      - Edit tab settings in prettier
    server:
      - Add sample express server
      - Add docker
      - update pnpm stripts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/health', (_req, res) => {
+  res.send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
