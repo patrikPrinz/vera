9e8a80517e0cb5afec74d58287ddc91a4d75ba6f
Patrik Prinz <patrik.prinz@patryk.cz>
2025-08-14 21:31:54 +0200

    Fix: eslint config imports
    root:
      - Fix imports in eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import { configs } from '@eslint/js';
+import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
-import { node, es2024 } from 'globals';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['**/node_modules/**', '**/dist/**', 'pnpm-lock.yaml', '.env*'] },
-  configs.recommended,
+  { ignores: ['**/node_modules/**', '**/dist/**', 'pnpm-lock.yaml', '.env*', 'eslint.config.js'] },
+  js.configs.recommended,
   {
     files: ['packages/*/src/**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-      globals: { ...node, ...es2024 },
+      globals: { ...globals.node, ...globals.es2024 },
     },
     rules: {},
   },
