4a2ae357040e877d8f19f3101525b246d6b5f58a
Patrik Prinz <patrik.prinz@patryk.cz>
2025-08-19 11:45:08 +0200

    Fix: add eslint type checking
    root:
      - edit eslint config to use basic recommended TypeScript rules for linting with type checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,28 @@
 import js from '@eslint/js';
+//import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { ignores: ['**/node_modules/**', '**/dist/**', 'pnpm-lock.yaml', '.env*', 'eslint.config.js'] },
+///** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+  { ignores: ['**/node_modules/**', '**/dist/**', 'pnpm-lock.yaml', '.env*'] },
   js.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
   {
     files: ['packages/*/src/**/*.ts'],
     languageOptions: {
       parser: tsParser,
-      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
       globals: { ...globals.node, ...globals.es2024 },
     },
     rules: {},
   },
   prettier,
-];
+);
