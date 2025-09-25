6eee3e4d9556e316a222f46cc1362175562432a3
Patrik Prinz <patrik.prinz@patryk.cz>
2025-09-25 23:30:36 +0200

    refactor: style changes

--- packages/elastic-initializer/src/index.ts
+++ packages/elastic-initializer/src/index.ts
@@ -3,7 +3,7 @@ import { Client } from '@elastic/elasticsearch';
 import type { estypes } from '@elastic/elasticsearch';
 
 const connection = new Client({
-  node: 'http://127.0.0.1:9200'
+  node: 'http://127.0.0.1:9200',
 });
 
 const MAPPINGS: estypes.IndicesCreateRequest[] = [
@@ -11,16 +11,16 @@ const MAPPINGS: estypes.IndicesCreateRequest[] = [
     index: 'bible',
     mappings: {
       properties: {
-        language: {type: 'keyword'},
-        translation: {type: 'keyword'},
-        book: {type: 'keyword'},
-        chapter: {type: 'integer'},
-        verse: {type: 'integer'},
-        is_header: {type: 'boolean'},
-        text: {type: 'text'}
-      }
-    }
-  }
+        language: { type: 'keyword' },
+        translation: { type: 'keyword' },
+        book: { type: 'keyword' },
+        chapter: { type: 'integer' },
+        verse: { type: 'integer' },
+        is_header: { type: 'boolean' },
+        text: { type: 'text' },
+      },
+    },
+  },
 ];
 
 for (const mapping of MAPPINGS) {
--- packages/elastic-initializer/src/lib/elasticsearch/elastic.ts
+++ packages/elastic-initializer/src/lib/elasticsearch/elastic.ts
@@ -1,8 +1,11 @@
 import { Client } from '@elastic/elasticsearch';
 import type { estypes } from '@elastic/elasticsearch';
 
-export async function initializeIndex(connection: Client, index: estypes.IndicesCreateRequest): Promise<void> {
-  const exists = await connection.indices.exists({'index': index.index});
+export async function initializeIndex(
+  connection: Client,
+  index: estypes.IndicesCreateRequest,
+): Promise<void> {
+  const exists = await connection.indices.exists({ index: index.index });
   if (!exists) {
     await connection.indices.create(index);
   }
