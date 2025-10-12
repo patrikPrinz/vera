99b9e83a569a2c5021948a7e427961fd7a0092d3
Patrik Prinz <patrik.prinz@patryk.cz>
2025-10-12 21:58:23 +0200

    build: unit test setup

---
+++ packages/server/jest.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+      useESM: true,
+    },
+  },
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+};
---
+++ packages/server/src/modules/bible/translation_parser/translation_parser.test.ts
@@ -0,0 +1,3 @@
+test('Test testing', () => {
+    expect(2).toBe(2);
+});
