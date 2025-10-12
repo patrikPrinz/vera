517c36a68d8e787a167128f647f80302c04737b0
Patrik Prinz <patrik.prinz@patryk.cz>
2025-10-12 21:23:05 +0200

    refactor: update parser structure
    Separate potentional common parser class structure into abstract class
    to make implementing of new parsers more convenient.

--- packages/server/src/modules/bible/translation_parser/translation_parser.ts
+++ packages/server/src/modules/bible/translation_parser/translation_parser.ts
@@ -3,68 +3,77 @@ import { z } from 'zod';
 
 import { BibleVerse } from '../bible.types.js';
 
-export interface TranslationParser {
-  getData(): Promise<Array<BibleVerse>>;
-}
-
-const schemaXml = z.object({
-  XMLBIBLE: z.object({
-    INFORMATION: z.array(
-      z.object({
-        identifier: z.array(z.string()),
-      }),
-    ),
-    BIBLEBOOK: z.array(
-      z.object({
-        $: z.object({
-          bnumber: z.string(),
-        }),
-        CHAPTER: z.array(
-          z.object({
-            $: z.object({
-              cnumber: z.string(),
-            }),
-            VERS: z.array(
-              z.object({
-                $: z.object({
-                  vnumber: z.string(),
-                }),
-                _: z.string(),
-              }),
-            ),
-          }),
-        ),
-      }),
-    ),
-  }),
-});
-export class TranslationParserXml implements TranslationParser {
+abstract class TranslationParser {
   protected rawData: string;
-  protected data: Array<BibleVerse>;
+  protected data: BibleVerse[];
 
   public constructor(data: string) {
     this.data = [];
     this.rawData = data;
   }
 
-  protected async parseData(): Promise<Array<BibleVerse>> {
+  public async getData(): Promise<BibleVerse[]> {
+    if (this.data.length == 0) {
+      this.data = await this.parseData();
+    }
+    return this.data;
+  }
+
+  protected abstract parseData(): Promise<BibleVerse[]>;
+}
+
+export class TranslationParserXml extends TranslationParser {
+  protected readonly schema = z.object({
+    XMLBIBLE: z.object({
+      INFORMATION: z.array(
+        z.object({
+          identifier: z.array(z.string()),
+        }),
+      ),
+      BIBLEBOOK: z.array(
+        z.object({
+          $: z.object({
+            bnumber: z.string(),
+          }),
+          CHAPTER: z.array(
+            z.object({
+              $: z.object({
+                cnumber: z.string(),
+              }),
+              VERS: z.array(
+                z.object({
+                  $: z.object({
+                    vnumber: z.string(),
+                  }),
+                  _: z.string(),
+                }),
+              ),
+            }),
+          ),
+        }),
+      ),
+    }),
+  });
+
+  protected async parseData(): Promise<BibleVerse[]> {
     try {
       const parser = new xml2js.Parser();
       const verses: Array<BibleVerse> = [];
       const parsedDocument: unknown = await parser.parseStringPromise(
         this.rawData,
       );
-      if (!schemaXml.parse(parsedDocument)) {
+
+      if (!this.schema.parse(parsedDocument)) {
         throw new Error();
       }
-      const parsedData = schemaXml.parse(parsedDocument).XMLBIBLE;
+
+      const parsedData = this.schema.parse(parsedDocument).XMLBIBLE;
       const translationCode = parsedData.INFORMATION[0].identifier[0];
-      console.log(parsedData.BIBLEBOOK);
+
       parsedData.BIBLEBOOK.forEach((book) => {
         const bookId: unknown = book.$.bnumber;
         book.CHAPTER.forEach((chapter) => {
           const chapterId: unknown = chapter.$.cnumber;
-          console.log(chapterId);
           chapter.VERS.forEach((verse) => {
             const verseId: unknown = verse.$.vnumber;
             const verseData = {
@@ -79,19 +88,10 @@ export class TranslationParserXml implements TranslationParser {
           });
         });
       });
-      console.log('ggg');
-      console.log(verses);
       return verses;
     } catch (err) {
       console.trace(err);
       throw new Error('Failed parsing XML. Structure is invalid.');
     }
-  }
-
-  public async getData(): Promise<Array<BibleVerse>> {
-    if (this.data.length == 0) {
-      this.data = await this.parseData();
-    }
-    return this.data;
   }
 }
