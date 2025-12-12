136d0f3e066940e2ec06821f38fca2cd54d1036c
Patrik Prinz <patrik.prinz@patryk.cz>
2025-12-12 10:03:39 +0100

    fix: add cors allow policy to server

--- packages/server/src/bootstrap.ts
+++ packages/server/src/bootstrap.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import type { Express } from 'express';
+import cors from 'cors';
 
 import { router as bibleRouter } from './modules/bible/index.js';
 import { errorHandler } from './shared/error_handler/error_handler.js';
 
 const app: Express = express();
+
+app.use(cors());
 
 app.use('/api/bible', bibleRouter);
 
