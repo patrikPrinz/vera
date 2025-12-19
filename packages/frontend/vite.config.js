64c0254eddf82a267a48dd7f44bfd168c7a38bf2
Patrik Prinz <patrik.prinz@patryk.cz>
2025-12-19 19:52:53 +0100

    build: frontend setup
    - remove unused tailwind and postcss config files
    - install missing dependencies for tailwind v4 with vite
    - extend tailwind config with dark theme and custom palette
    - edit linter configuration for vue js files to accept browser globals

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,21 +1,22 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   server: {
-    host: true,     // = 0.0.0.0
+    host: true, // = 0.0.0.0
     port: 3001,
     watch: {
       usePolling: true,
-      interval: 100
-    }
-  }
+      interval: 100,
+    },
+  },
 });
