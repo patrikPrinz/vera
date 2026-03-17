16a4e1487140a3a6c3124a1c890a32ccce38e099
Patrik Prinz <patrik.prinz@patryk.cz>
2026-03-17 12:09:39 +0100

    fix: UI improvements
    - Improve Bible menu modals
    - Fix z-index issues with side bar
    - Add register link into login view

--- packages/frontend/src/modules/bible/components/modals/NoteModal.vue
+++ packages/frontend/src/modules/bible/components/modals/NoteModal.vue
@@ -1,13 +1,23 @@
 <template>
   <VueFinalModal
-    class="flex items-end justify-start bg-transparent"
-    content-class="mb-4 ml-4 bg-gray-800 rounded p-4 shadow"
+    class="flex items-center justify-center bg-transparent"
+    content-class="mb-4 ml-4 bg-secondary rounded p-4 shadow"
   >
     <form class="mb-2 flex gap-2">
-      <textarea v-model="insertedNote" class="bg-sky-100"></textarea>
+      <textarea
+        width="100px"
+        height="100px"
+        v-model="insertedNote"
+        class="bg-sky-100"
+      ></textarea>
     </form>
 
-    <button @click="submit" class="text-amber-50">Ok</button>
+    <button
+      @click="submit"
+      class="bg-primary cursor-pointer rounded p-1 px-3 text-xl text-amber-50 hover:border-slate-950"
+    >
+      Ok
+    </button>
   </VueFinalModal>
 </template>
 
