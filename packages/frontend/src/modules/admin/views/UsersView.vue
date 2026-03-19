8f5aca8cec5c1e3b86460c6ff3c22c6e67aa11f0
Patrik Prinz <patrik.prinz@patryk.cz>
2026-03-19 13:49:14 +0100

    fix: Minor UI improvements
    - remove redundant key binding from register screen
    - minor style and UX improvements, internationalization changes

--- packages/frontend/src/modules/admin/views/UsersView.vue
+++ packages/frontend/src/modules/admin/views/UsersView.vue
@@ -1,13 +1,13 @@
 <template>
-  <router-link to="/auth/register">
-    <BiSolidPlusSquare class="text-2xl"></BiSolidPlusSquare>
+  <router-link class="w-min" to="/auth/register">
+    <BiSolidPlusSquare class="h-auto w-8 cursor-pointer"></BiSolidPlusSquare>
   </router-link>
   <table class="table-auto, m-auto mt-5 w-1/2">
     <thead>
       <tr>
         <th>#</th>
-        <th>Name</th>
-        <th>Email</th>
+        <th>{{ i18n.t('general.email') }}</th>
+        <th>{{ i18n.t('general.name') }}</th>
       </tr>
     </thead>
     <tbody>
@@ -16,30 +16,36 @@
         <td>{{ user.email }}</td>
         <td>{{ user.username ?? '-' }}</td>
         <td>
-          <BiPen
+          <button
+            :title="i18n.t('admin.manageRoles')"
             class="cursor-pointer text-xl"
             @click="openURolesModal(user.id)"
-          ></BiPen>
+          >
+            <BiEdit></BiEdit>
+          </button>
         </td>
-        <td>
-          <BiRefresh
-            class="cursor-pointer text-xl"
-            @click="resetPassword(user.id)"
-          ></BiRefresh>
-        </td>
+        <button
+          :title="i18n.t('admin.resetPassword')"
+          class="cursor-pointer text-xl"
+          @click="resetPassword(user.id)"
+        >
+          <BiRefresh />
+        </button>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script setup lang="ts">
-import { BiSolidPlusSquare, BiRefresh, BiPen } from 'vue-icons-plus/bi';
+import { BiSolidPlusSquare, BiRefresh, BiEdit } from 'vue-icons-plus/bi';
 import { onBeforeMount, ref, type Ref } from 'vue';
 import type { UserDetails } from '@/shared/types/auth/auth.types';
 import { adminService } from '../services/adminService.provider';
 import ManageRolesModal from '../components/modals/ManageRolesModal.vue';
 import { useModal } from 'vue-final-modal';
+import { useI18n } from 'vue-i18n';
 
+const i18n = useI18n();
 const users: Ref<UserDetails[]> = ref([]);
 
 onBeforeMount(async () => {
