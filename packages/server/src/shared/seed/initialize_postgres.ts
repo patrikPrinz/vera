ea437aa014f667e53eae12453a28ec0b667d7d59
Patrik Prinz <patrik.prinz@patryk.cz>
2026-04-18 18:58:43 +0200

    fix: Typo in env variable

--- packages/server/src/shared/seed/initialize_postgres.ts
+++ packages/server/src/shared/seed/initialize_postgres.ts
@@ -6,7 +6,7 @@ export async function initializePostgres(
   logger: WinstonLogger,
 ) {
   const login = process.env.APP_ADMIN_USERNAME ?? 'user';
-  const password = process.env.APP_ADMIN_APSSWORD ?? 'password';
+  const password = process.env.APP_ADMIN_PASSWORD ?? 'password';
   logger.info('Performing database seed...');
   const insertion = await service.seedAdminUser(login, password);
   if (insertion) {
